797075348d3a9581251ac3f78b729e4d6a4d6837
Maycon Satel <maycon.gc@hotmail.com>
2021-02-27 17:33:34 -0300

    att avatar profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -7,7 +7,7 @@ export function Profile() {
     
     return (
         <div className={styles.profileContainer}>
-            <img src="https://image.freepik.com/vetores-gratis/perfil-de-avatar-de-homem-no-icone-redondo_24640-14044.jpg" alt="Maycon Satel"/>
+            <img src="https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" alt="Maycon Satel"/>
             <div>
                 <strong>Jogador</strong>
                 <p>
